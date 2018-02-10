d5dbdc5b793afe941e111abe0bc8b009bea57e05
iNetBlazer <btmcclur@geneva.edu>
2018-02-10 00:57:10 -0500

    again...

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(morgan('combined'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = "mongodb://iNetBlazer:ZAQ!xsw2CDE#vfr4@inetblazercluster0-shard-00-00-p5eu2.mongodb.net:27017,inetblazercluster0-shard-00-01-p5eu2.mongodb.net:27017,inetblazercluster0-shard-00-02-p5eu2.mongodb.net:27017/test?ssl=true&replicaSet=iNetBlazerCluster0-shard-0&authSource=admin",
+    mongoURL = "mongodb://iNetBlazer:ZAQ%21xsw2CDE%23vfr4@inetblazercluster0-shard-00-00-p5eu2.mongodb.net%3A27017%2Cinetblazercluster0-shard-00-01-p5eu2.mongodb.net%3A27017%2Cinetblazercluster0-shard-00-02-p5eu2.mongodb.net%3A27017%2Ftest%3Fssl%3Dtrue%26replicaSet%3DiNetBlazerCluster0-shard-0%26authSource%3Dadmin",
     mongoURLLabel = "";
 
 if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
